43cdfa0674125d1d39910b81bc84bf4f49b92bf7
Xin <xye@centronelson.org>
2023-04-28 11:37:19 +0200

    Primer commit rama2

--- src/Main.java
+++ src/Main.java
@@ -54,4 +54,15 @@ public class Main {
         int resultado = a * b;
         System.out.println("La multiplicación de " + a + " y " + b + " es: " + resultado);
     }
+    
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
 }
