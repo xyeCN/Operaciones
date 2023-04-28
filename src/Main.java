8c38ec43899d12cd07b9117d6f7232679accfacc
Xin <xye@centronelson.org>
2023-04-28 10:12:57 +0200

    Primer commit rama1

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,23 @@ public class Main {
         
         do {
             System.out.println("Seleccione una opción:");
-            System.out.println("1. Sumar dos números");
-            System.out.println("2. Multiplicar dos números");
+            System.out.println("1. Sumar números");
+            System.out.println("2. Multiplicar números");
             System.out.println("0. Salir");
             opcion = scanner.nextInt();
             
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingrese el primer número:");
-                    int numero1 = scanner.nextInt();
-                    System.out.println("Ingrese el segundo número:");
-                    int numero2 = scanner.nextInt();
-                    sumar2(numero1, numero2);
+                    System.out.println("Ingrese la cantidad de números a sumar:");
+                    int cantidadSumar = scanner.nextInt();
+                    int[] numerosSumar = ingresarNumeros(cantidadSumar);
+                    sumarN(numerosSumar);
                     break;
                 case 2:
-                    System.out.println("Ingrese el primer número:");
-                    int numero3 = scanner.nextInt();
-                    System.out.println("Ingrese el segundo número:");
-                    int numero4 = scanner.nextInt();
-                    multiplicar2(numero3, numero4);
+                    System.out.println("Ingrese la cantidad de números a multiplicar:");
+                    int cantidadMultiplicar = scanner.nextInt();
+                    int[] numerosMultiplicar = ingresarNumeros(cantidadMultiplicar);
+                    multiplicarN(numerosMultiplicar);
                     break;
                 case 0:
                     System.out.println("Saliendo del programa...");
@@ -45,13 +43,49 @@ public class Main {
         scanner.close();
     }
     
-    public static void sumar2(int a, int b) {
-        int resultado = a + b;
-        System.out.println("La suma de " + a + " y " + b + " es: " + resultado);
+    public static int[] ingresarNumeros(int cantidad) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[cantidad];
+        
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Ingrese el número " + (i + 1) + ":");
+            numeros[i] = scanner.nextInt();
+        }
+        
+        return numeros;
     }
     
-    public static void multiplicar2(int a, int b) {
-        int resultado = a * b;
-        System.out.println("La multiplicación de " + a + " y " + b + " es: " + resultado);
+    public static void sumarN(int[] numeros) {
+        int resultado = 0;
+        
+        for (int numero : numeros) {
+            resultado += numero;
+        }
+        
+        System.out.print("La suma de los números ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]);
+            if (i != numeros.length - 1) {
+                System.out.print(" + ");
+            }
+        }
+        System.out.println(" es: " + resultado);
+    }
+    
+    public static void multiplicarN(int[] numeros) {
+        int resultado = 1;
+        
+        for (int numero : numeros) {
+            resultado *= numero;
+        }
+        
+        System.out.print("La multiplicación de los números ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]);
+            if (i != numeros.length - 1) {
+                System.out.print(" * ");
+            }
+        }
+        System.out.println(" es: " + resultado);
     }
 }
